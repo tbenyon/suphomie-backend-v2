60f4d9c4e17df40e665e8b8def0d046b226b4d9f
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-03-12 08:59:52 +0000

    Add the use of a config file to keep secret keys hidden from the repo
    Added example config file to support others using the app

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var Pusher = require('pusher');
+var Config = require("./config.json");
 
-var pusher = new Pusher({
-    appId: '313215',
-    key: 'b088b407ba235be620a1',
-    secret: '6ddcae279b1f84de7f00',
-    cluster: 'eu',
-    encrypted: true
-});
+var pusher = new Pusher(Config.pusher);
 
 pusher.trigger('my-channel', 'my-event', {
     "message": "hello world"
