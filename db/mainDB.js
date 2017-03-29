45f6e3acae6c778dff8923aded0fc9fd7a41e964
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-03-29 16:47:36 +0100

    Update the Mongo structure to wrap it in a Promise

--- db/mainDB.js
+++ db/mainDB.js
@@ -4,33 +4,37 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017/test';
 
 const sh_insert = function (data) {
-    MongoClient.connect(url).then(function (db) {
-        return insertDocuments(db, data);
-    }).then(function (response) {
-        console.log("Data added successfully.");
-        response.db.close();
-    }).catch(function (err) {
-        console.log(err);
-    });
-
-    const insertDocuments = function(db, data) {
-        return new Promise( function (resolve, reject) {
-            const collection = db.collection('status');
-            collection.insertMany(data).then(function (response) {
-                response.db = db;
-                resolve(response);
-            }).catch(function (err) {
-                reject(err)
-            });
+    return new Promise(function (resolve, reject) {
+        MongoClient.connect(url).then(function (db) {
+            return insertDocuments(db, data);
+        }).then(function (response) {
+            response.db.close();
+            resolve("Data added successfully.");
+        }).catch(function (err) {
+            reject(err);
         });
-    };
+
+        const insertDocuments = function(db, data) {
+            return new Promise( function (resolve, reject) {
+                const collection = db.collection('status');
+                collection.insertMany(data).then(function (response) {
+                    response.db = db;
+                    resolve(response);
+                }).catch(function (err) {
+                    reject(err)
+                });
+            });
+        };
+    })
 };
 
 const data = [{
     central_heating: {
         heating: true,
-        water: false
+        water: true
     }
 }];
 
-sh_insert(data);
+sh_insert(data).then(function (result) {
+    console.log(result);
+});
