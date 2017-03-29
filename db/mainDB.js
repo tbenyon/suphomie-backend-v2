68fd6ea83c83bb1c2092949caaacf829b5f8e518
Tom Benyon <Tom.Benyon@creative-assembly.com>
2017-03-29 16:39:41 +0100

    Restructure Mongo Inserting Data

--- db/mainDB.js
+++ db/mainDB.js
@@ -1,10 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
+const url = 'mongodb://localhost:27017/test';
 
-MongoClient.connect(url).then(function (db) {
-        return insertDocuments(db);
+const sh_insert = function (data) {
+    MongoClient.connect(url).then(function (db) {
+        return insertDocuments(db, data);
     }).then(function (response) {
         console.log("Data added successfully.");
         response.db.close();
@@ -12,22 +13,24 @@ MongoClient.connect(url).then(function (db) {
         console.log(err);
     });
 
-var insertDocuments = function(db) {
-    return new Promise( function (resolve, reject) {
-        var collection = db.collection('status');
-
-        data = {
-            central_heating: {
-                heating: false,
-                water: false
-            }
-        };
-
-        collection.insert(data).then(function (response) {
-            response.db = db;
-            resolve(response);
-        }).catch(function (err) {
-            reject(err)
+    const insertDocuments = function(db, data) {
+        return new Promise( function (resolve, reject) {
+            const collection = db.collection('status');
+            collection.insertMany(data).then(function (response) {
+                response.db = db;
+                resolve(response);
+            }).catch(function (err) {
+                reject(err)
+            });
         });
-    });
+    };
 };
+
+const data = [{
+    central_heating: {
+        heating: true,
+        water: false
+    }
+}];
+
+sh_insert(data);
